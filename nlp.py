fca95032c1d0d711318bc62692d2b0090eceef4e
shenfanyi <shenfanyi96@gmail.com>
2018-05-04 11:42:19 +0800

    get way1 result(1.csv)

--- nlp.py
+++ nlp.py
@@ -9,6 +9,7 @@ import jieba.analyse
 import jieba.posseg
 import os
 from operator import itemgetter
+import pandas as pd
 
 
 
@@ -17,16 +18,18 @@ files = []
 for fpathe, dirs, fs in os.walk('corpora/Sogou'):
     for f in fs:
         files.append(os.path.join(fpathe, f))
-print(len(files))  # len(files) = 17910
+print(len(files))  # len(files) = 17,910
 
 
 
 # ----read data and transfer coding way: GB2312 to UTF-8----
-text = ''
 # with open(files[0], 'rb') as f:
 #     print chardet.detect(f.readline())["encoding"]
-n_files = 100
-for file in files[:n_files]:
+
+text = ''
+# n_files = 10
+# for file in files[:n_files]:
+for file in files:
     with open(file, 'rb') as f:
         for i in f.readlines():
             text = text + i
@@ -35,26 +38,35 @@ for file in files[:n_files]:
 
 
 # ----cut text: jieba cut----
-seg_list = jieba.cut(text)
+seg_list = jieba.cut(text, cut_all=False)
 
 
 
 # ----keyword: extract, index, weight----
-# way1: tf-idf
-n1 = 0
-for x, w in jieba.analyse.extract_tags(text, topK=10000, withWeight=True):
-    print '%s %s' % (x, w)
-    n1 += 1
-print n1
+# --- way1: tf-idf
+keywords_weighted = []
+for x, w in jieba.analyse.extract_tags(text, topK=10000000, withWeight=True, allowPOS=('ns', 'n', 'vn', 'v')):
+    # print '%s %s' % (x, w)
+    keywords_weighted.append([x.encode('UTF-8'),w])
 
-# # way2: TextRank
+# --save to .csv
+keywords_weighted_df = pd.DataFrame(keywords_weighted, columns=['keyword','weight'])
+keywords_weighted_df.to_csv('output/keyword_weight_1.csv')
+# print(keywords_weighted_df)
+# --check csv
+# keywords_weighted_df = pd.read_csv('output/keyword_weight_1.csv')
+# print(keywords_weighted_df.size)  # 364,755
+
+
+# --- way2: TextRank
 # n2 = 0
-# for x, w in jieba.analyse.textrank(text, topK=10000, withWeight=True):
+# for x, w in jieba.analyse.textrank(text, topK=1000000, withWeight=True):
 #     print '%s %s' % (x, w)
 #     n2 += 1
 # print n2
 
-# # way3: according times only, from cutted text
+
+# --- way3: according times only, from cutted text
 # dict = {}
 # stop_word_list = [U'', U' ', U'  ', U'   ', U'、', U'，', U'。', U'的']
 # for word in seg_list:
