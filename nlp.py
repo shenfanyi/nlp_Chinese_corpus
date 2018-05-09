2b502a880d114c5700c0eb92857fecb9f205b5e4
shenfanyi <shenfanyi96@gmail.com>
2018-05-09 15:37:42 +0800

    add POS,group v,n,adj,adv

--- nlp.py
+++ nlp.py
@@ -18,7 +18,8 @@ files = []
 for fpathe, dirs, fs in os.walk('corpora/Sogou'):
     for f in fs:
         files.append(os.path.join(fpathe, f))
-print(len(files))  # len(files) = 17,910
+        # break
+print('the total number of files: ', len(files))  # len(files) = 17,910
 
 
 
@@ -44,15 +45,30 @@ seg_list = jieba.cut(text, cut_all=False)
 
 # ----keyword: extract, index, weight----
 # --- way1: tf-idf
-keywords_weighted = []
-for x, w in jieba.analyse.extract_tags(text, topK=10000000, withWeight=True, allowPOS=('ns', 'n', 'vn', 'v')):
+allwords_sorted = []
+for x, w in jieba.analyse.extract_tags(text, topK=10000000, withWeight=True, withFlag=True, allowPOS=('n', 'ns', 'nr', 'v', 'a', 'ad')):
     # print '%s %s' % (x, w)
-    keywords_weighted.append([x.encode('UTF-8'),w])
+    allwords_sorted.append([x.encode('UTF-8').split('/')[0], x.encode('UTF-8').split('/')[1], w])
 
 # --save to .csv
-keywords_weighted_df = pd.DataFrame(keywords_weighted, columns=['keyword','weight'])
-keywords_weighted_df.to_csv('output/keyword_weight_1.csv')
-# print(keywords_weighted_df)
+allwords_sorted_df = pd.DataFrame(allwords_sorted, columns=['keyword', 'pos', 'weight'])
+noun_sorted_df = allwords_sorted_df[allwords_sorted_df.pos.isin(['n', 'ns', 'nr'])]
+verb_sorted_df = allwords_sorted_df[allwords_sorted_df.pos == 'v']
+adj_sorted_df = allwords_sorted_df[allwords_sorted_df.pos == 'a']
+adv_sorted_df = allwords_sorted_df[allwords_sorted_df.pos == 'ad']
+
+print('allwords number: ', len(allwords_sorted_df))  # 162,105
+print('noun number: ', len(noun_sorted_df))  # 124,353
+print('verb number: ', len(verb_sorted_df))  # 33,217
+print('adj number: ', len(adj_sorted_df))  # 4,420
+print('adv number: ', len(adv_sorted_df))  # 115
+
+allwords_sorted_df.to_csv('output/allwords_sorted.csv')
+noun_sorted_df.to_csv('output/noun_sorted.csv')
+verb_sorted_df.to_csv('output/verb_sorted.csv')
+adj_sorted_df.to_csv('output/adj_sorted.csv')
+adv_sorted_df.to_csv('output/adv_sorted.csv')
+
 # --check csv
 # keywords_weighted_df = pd.read_csv('output/keyword_weight_1.csv')
 # print(keywords_weighted_df.size)  # 364,755
@@ -84,4 +100,7 @@ keywords_weighted_df.to_csv('output/keyword_weight_1.csv')
 # for key, value in dict_sorted:
 #     print("{} : {}".format(key.encode('UTF-8'), value))
 # print(len(dict))
+
+
+
 
